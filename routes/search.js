69b05d25a19db57c7b2a6dcb197b1894451a99f5
Naveen Yadav <greatguy494@gmail.com>
2022-04-11 21:54:45 +0530

    repo created

---
+++ routes/search.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+const searchController = require('../controllers/search');
+
+router.get("/search", searchController.searchpost);
+
+module.exports = router;
